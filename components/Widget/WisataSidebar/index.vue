0cfa41dc00a99d1adf1f3f1071d97f667853e1b3
ryunitou <ryunitou@gmail.com>
2022-09-12 15:35:30 +0700

    update wistata

---
+++ components/Widget/WisataSidebar/index.vue
@@ -0,0 +1,213 @@
+<template>
+  <div class="h-44 w-full">
+    &nbsp;
+    <div
+      :style="backgroundnize"
+      class="fixed -top-24 w-full left-0"
+      style="z-index: -1"
+    ></div>
+    <!-- <img :src="img" class="fixed -top-24 w-full left-0" style="z-index: -1" /> -->
+    <div
+      class="
+        pt-10
+        pb-12
+        pr-12
+        pl-24
+        w-5/12
+        h-full
+        absolute
+        right-0
+        top-0
+        flex
+        baseBrowserHeight
+        items-center
+      "
+      :style="{ background: backgroundColor }"
+    >
+      <div class="w-9/12" :style="{ color: color }">
+        <div class="font-bold text-5xl">
+          {{ name !== '' ? name : 'No Name of location' }}
+        </div>
+        <hr class="w-32 mt-3" style="border-color: #b6b6b6" />
+        <div class="text-sm mt-3 w-11/12">
+          {{ desc !== '' ? desc : 'No Data' }}
+        </div>
+      </div>
+      <div
+        class="w-3/12 flex mx-auto space-x-4 items-center"
+        v-if="forecast.length > 0"
+      >
+        <div class="flex-none">
+          <img
+            :src="'/Archive/' + forecast[0].weather_code + '.svg'"
+            class="w-24 mx-auto"
+            alt="imgdata"
+          />
+        </div>
+        <div class="flex-grow" :style="{ color: color }">
+          <div class="text-6xl text-left" style="font-weight: 200 !important">
+            <div>{{ forecast[0].temp }}<sup>o</sup>C</div>
+          </div>
+          <div class="relative text-lg left-5">
+            <div class="">{{ weather_code[forecast[0].weather_code] }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { weather_code } from '../../../utils/helperNDF.js'
+export default {
+  data() {
+    return {
+      name: '',
+      desc: '',
+      forecast: [],
+      color: '#000000',
+      backgroundColor: '#ffffff',
+      img: '',
+      backgroundnize: {},
+      allNDF: {},
+      idTemplate: null 
+    }
+  },
+  computed: {
+    weather_code: function () {
+      return weather_code
+    },
+  },
+  methods: {
+    async getData() {
+      var self = this
+      var parentDisplay = self.$parent.$parent.$parent
+      if (parentDisplay.production) {
+        this.allNDF = {}
+        var ndflistener = this.allNDF
+        var setting = parentDisplay.responseDisplay.properties.allSetting
+        var obj = parentDisplay.obj.idtemplate
+        setting[obj].forEach(async (el) => {
+          var key = el.key.split('_')[2]
+          if (key == 'subdistrict') {
+            // console.log(this.ndflistener[el.value.ndf], el)
+            const datares = await this.$axios.$get(
+              'https://weather.circlegeo.com/api/cgms/weather/ndf/get?locationId=' +
+                el.value.ndf
+            )
+
+            self.$set(ndflistener, el.value.ndf, datares.data)
+            // ndflistener[el.value.ndf] = datares.data
+
+            if (ndflistener[el.value.ndf].length > 0) {
+              for (var i = 0; i < ndflistener[el.value.ndf].length; i++) {
+                var comp = ndflistener[el.value.ndf][i]
+                // if (comp.date.split('T')[1].split(':')[0] == '12') {
+                this.forecast.push(comp)
+                break
+                // }
+              }
+            }
+          }
+        })
+      }
+    },
+  },
+  mounted() {
+    var parentDisplay = this.$parent.$parent.$parent
+    this.idTemplate = parentDisplay.obj && parentDisplay.obj.idtemplate
+
+    if (parentDisplay.production) {
+      var setting = parentDisplay.responseDisplay.properties.allSetting
+      var obj = parentDisplay.obj.idtemplate
+      setting[obj].forEach((el) => {
+        var key = el.key.split('_')[2]
+        if (key == 'name') {
+          this.name = el.value
+        } else if (key == 'subdistrict') {
+          this.forecast.length = 0
+          this.getData()
+        } else if (key == 'description') {
+          this.desc = el.value
+        } else if (key == 'color') {
+          this.color = el.value
+        } else if (key == 'background') {
+          this.backgroundColor = el.value
+        } else if (key == 'img') {
+          this.img = el.value
+          this.backgroundnize = {
+            'background-image': 'url(' + this.img + ')',
+            'background-size': 'cover',
+            'background-position': 'center',
+            'background-repeat': 'no-repeat',
+            height: '100%',
+          }
+        }
+      })
+
+      setInterval(() => {
+        this.forecast.length = 0
+        this.getData()
+      }, 3600000)
+    } else {
+      
+      if (
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_description'
+        ]
+      ) {
+        this.desc =
+          this.$store.state.displayWidget.widgetSaved[
+            this.idTemplate + '_WidgetWisataSidebar_description'
+          ]
+      }
+      if (
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_name'
+        ]
+      ) {
+        this.name =
+          this.$store.state.displayWidget.widgetSaved[
+            this.idTemplate + '_WidgetWisataSidebar_name'
+          ]
+      }
+      if (
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_color'
+        ]
+      ) {
+        this.color =
+          this.$store.state.displayWidget.widgetSaved[
+            this.idTemplate + '_WidgetWisataSidebar_color'
+          ]
+      }
+      if (
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_background'
+        ]
+      ) {
+        this.backgroundColor =
+          this.$store.state.displayWidget.widgetSaved[
+            this.idTemplate + '_WidgetWisataSidebar_background'
+          ]
+      }
+      if (
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_img'
+        ]
+      ) {
+        this.img =
+          this.$store.state.displayWidget.widgetSaved[
+            this.idTemplate + '_WidgetWisataSidebar_img'
+          ]
+      }
+    }
+  },
+}
+</script>
+
+<style scoped>
+.font-color {
+  color: #4e256c;
+}
+</style>
---
+++ components/Widget/WisataSidebar/setting.vue
@@ -0,0 +1,225 @@
+<template>
+  <div>
+    <div>Province</div>
+    <div>
+      <v-select
+        label="name"
+        @option:selected="changeSelected"
+        v-model="province"
+        :options="listProvinsi"
+      ></v-select>
+    </div>
+    <div class="mt-2">Sub District</div>
+    <div>
+      <v-select
+        label="location"
+        @option:selected="changeSelectedSub"
+        v-model="subdistrict"
+        :options="listAll"
+      ></v-select>
+    </div>
+    <div class="mt-2">Name of location</div>
+    <div>
+      <input
+        type="text"
+        class="px-2 py-1.5 w-full border border-gray-300 rounded"
+        @input="changeName"
+        v-model="name"
+        placeholder="name place"
+      />
+    </div>
+    <div class="mt-2">Description</div>
+    <div>
+      <textarea
+        name="desc"
+        v-model="desc"
+        class="border border-gray-300 rounded p-2"
+        placeholder="input description here"
+        @input="changeDesc"
+        id="desc"
+        cols="30"
+        rows="10"
+      ></textarea>
+    </div>
+    <!-- changeImg -->
+    <div class="mt-2">Image Background URL</div>
+    <div>
+      <input
+        type="text"
+        class="px-2 py-1.5 w-full border border-gray-300 rounded"
+        @input="changeImg"
+        v-model="img"
+        placeholder="name place"
+      />
+    </div>
+    <div class="mt-2">Color</div>
+    <div>
+      <input
+        type="color"
+        class="px-2 py-1.5 w-full border border-gray-300 rounded"
+        @change="changeColor"
+        v-model="color"
+        placeholder="name place"
+      />
+    </div>
+    <div class="mt-2">Background Color</div>
+    <div>
+      <input
+        type="color"
+        class="px-2 py-1.5 w-full border border-gray-300 rounded"
+        @change="changeBackground"
+        v-model="backgroundColor"
+        placeholder="name place"
+      />
+    </div>
+  </div>
+</template>
+<script>
+import { dataStatic, belitung, belitungTimur } from '../../../utils/listStatic.js'
+export default {
+  props: {
+    idTemplate: {
+      default: () => {
+        return ''
+      },
+    },
+  },
+  methods: {
+    changeSelected() {
+      this.$store.commit('displayWidget/mutationWidget', {
+        key: this.idTemplate + '_WidgetWisataSidebar_province',
+        value: this.province,
+      })
+    },
+    changeSelectedSub() {
+      this.$store.commit('displayWidget/mutationWidget', {
+        key: this.idTemplate + '_WidgetWisataSidebar_subdistrict',
+        value: this.subdistrict,
+      })
+    },
+    changeDesc() {
+      this.$store.commit('displayWidget/mutationWidget', {
+        key: this.idTemplate + '_WidgetWisataSidebar_description',
+        value: this.desc,
+      })
+    },
+    changeName() {
+      this.$store.commit('displayWidget/mutationWidget', {
+        key: this.idTemplate + '_WidgetWisataSidebar_name',
+        value: this.name,
+      })
+    },
+    changeBackground() {
+      this.$store.commit('displayWidget/mutationWidget', {
+        key: this.idTemplate + '_WidgetWisataSidebar_background',
+        value: this.backgroundColor,
+      })
+    },
+    changeColor() {
+      this.$store.commit('displayWidget/mutationWidget', {
+        key: this.idTemplate + '_WidgetWisataSidebar_color',
+        value: this.color,
+      })
+    },
+    changeImg() {
+      this.$store.commit('displayWidget/mutationWidget', {
+        key: this.idTemplate + '_WidgetWisataSidebar_img',
+        value: this.img,
+      })
+    },
+  },
+  mounted() {
+    if (
+      this.$store.state.displayWidget.widgetSaved[
+        this.idTemplate + '_WidgetWisataSidebar_province'
+      ]
+    ) {
+      this.province =
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_province'
+        ]
+    }
+    if (
+      this.$store.state.displayWidget.widgetSaved[
+        this.idTemplate + '_WidgetWisataSidebar_subdistrict'
+      ]
+    ) {
+      this.subdistrict =
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_subdistrict'
+        ]
+    }
+    if (
+      this.$store.state.displayWidget.widgetSaved[
+        this.idTemplate + '_WidgetWisataSidebar_description'
+      ]
+    ) {
+      this.desc =
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_description'
+        ]
+    }
+    if (
+      this.$store.state.displayWidget.widgetSaved[
+        this.idTemplate + '_WidgetWisataSidebar_name'
+      ]
+    ) {
+      this.name =
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_name'
+        ]
+    }
+    if (
+      this.$store.state.displayWidget.widgetSaved[
+        this.idTemplate + '_WidgetWisataSidebar_color'
+      ]
+    ) {
+      this.color =
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_color'
+        ]
+    }
+    if (
+      this.$store.state.displayWidget.widgetSaved[
+        this.idTemplate + '_WidgetWisataSidebar_background'
+      ]
+    ) {
+      this.backgroundColor =
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_background'
+        ]
+    }
+    if (
+      this.$store.state.displayWidget.widgetSaved[
+        this.idTemplate + '_WidgetWisataSidebar_img'
+      ]
+    ) {
+      this.img =
+        this.$store.state.displayWidget.widgetSaved[
+          this.idTemplate + '_WidgetWisataSidebar_img'
+        ]
+    }
+  },
+  data() {
+    return {
+      province: '',
+      subdistrict: '',
+      desc: '',
+      name: '',
+      color: '#000000',
+      backgroundColor: '#ffffff',
+      img: '',
+      listProvinsi: [
+        {
+          name: 'Kepulauan Bangka Belitung',
+        },
+      ],
+      listWisata: dataStatic,
+      listBelitung: belitung,
+      listBelitungTimur: belitungTimur, 
+      listAll:[...belitung, ...belitungTimur, ...dataStatic],
+
+    }
+  },
+}
+</script>
