909ee5015e98a3b4dcd1f8bc97428f4e54bee26c
CuacaIndoor <cuacaindoor@ip-182-16-248-247.bmkg.go.id>
2022-12-28 03:14:30 -0500

    repair weather warning

--- components/Widget/WarningWeather/setting.vue
+++ components/Widget/WarningWeather/setting.vue
@@ -3,7 +3,7 @@
     <div>Area code</div>
     <div>
       <v-select
-        label="area"
+        label="name"
         @option:selected="changeSelected"
         v-model="area"
         :options="listArea"
@@ -46,76 +46,144 @@ export default {
   data() {
     return {
       area: '',
-      listArea: [{
-        // name: 'Bangka Belitung',
-        area: 'CBB'
-      }, {
-        area: 'CAH',
-      }, {
-        area: 'CBK',
-      }, {
-        area: 'CBL',
-      }, {
-        area: 'CBT',
-      }, {
-        area: 'CGT',
-      }, {
-        area: 'CJB',
-      }, {
-        area: 'CJH',
-      }, {
-        area: 'CJI',
-      }, {
-        area: 'CJK',
-      }, {
-        area: 'CJT',
-      }, {
-        area: 'CKB',
-      }, {
-        area: 'CKG',
-      }, {
-        area: 'CKR',
-      }, {
-        area: 'CKS',
-      }, {
-        area: 'CKT',
-      }, {
-        area: 'CKU',
-      }, {
-        area: 'CLG',
-      }, {
-        area: 'CML',
-      }, {
-        area: 'CMU',
-      }, {
-        area: 'CNB',
-      }, {
-        area: 'CNT',
-      }, {
-        area: 'CPB',
-      }, {
-        area: 'CPU',
-      }, {
-        area: 'CRU',
-      }, {
-        area: 'CSB',
-      }, {
-        area: 'CSG',
-      }, {
-        area: 'CSK',
-      }, {
-        area: 'CSL',
-      }, {
-        area: 'CSR',
-      }, {
-        area: 'CSS',
-      }, {
-        area: 'CST',
-      }, {
-        area: 'CSU',
-      }, {
-        area: 'CYG',
-      }],
+      listArea: [
+        {
+          "id": "CJK",
+          "name": "Jabodetabek"
+        },
+        {
+          "id": "CJB",
+          "name": "Jawa Barat"
+        },
+        {
+          "id": "CBT",
+          "name": "Banten"
+        },
+        {
+          "id": "CYG",
+          "name": "Daerah Istimewa Yogyakarta"
+        },
+        {
+          "id": "CJH",
+          "name": "Jawa Tengah"
+        },
+        {
+          "id": "CJT",
+          "name": "Jawa Timur"
+        },
+        {
+          "id": "CSU",
+          "name": "Sumatera Utara"
+        },
+        {
+          "id": "CPU",
+          "name": "Papua"
+        },
+        {
+          "id": "CSL",
+          "name": "Sulawesi Selatan"
+        },
+        {
+          "id": "CAH",
+          "name": "Aceh"
+        },
+        {
+          "id": "CBK",
+          "name": "Bengkulu"
+        },
+        {
+          "id": "CJI",
+          "name": "Jambi"
+        },
+        {
+          "id": "CBB",
+          "name": "Bangka Belitung"
+        },
+        {
+          "id": "CKR",
+          "name": "Kepulauan Riau"
+        },
+        {
+          "id": "CLG",
+          "name": "Lampung"
+        },
+        {
+          "id": "CRU",
+          "name": "Riau"
+        },
+        {
+          "id": "CSB",
+          "name": "Sumatera Barat"
+        },
+        {
+          "id": "CGT",
+          "name": "Gorontalo"
+        },
+        {
+          "id": "CSK",
+          "name": "Sulawesi Barat"
+        },
+        {
+          "id": "CSS",
+          "name": "Sumatera Selatan"
+        },
+        {
+          "id": "CSG",
+          "name": "Sulawesi Tengah"
+        },
+        {
+          "id": "CST",
+          "name": "Sulawesi Tenggara"
+        },
+        {
+          "id": "CSR",
+          "name": "Sulawesi Utara"
+        },
+        {
+          "id": "CML",
+          "name": "Maluku"
+        },
+        {
+          "id": "CMU",
+          "name": "Maluku Utara"
+        },
+        {
+          "id": "CPB",
+          "name": "Papua Barat"
+        },
+        {
+          "id": "CKB",
+          "name": "Kalimantan Barat"
+        },
+        {
+          "id": "CKS",
+          "name": "Kalimantan Selatan"
+        },
+        {
+          "id": "CKG",
+          "name": "Kalimantan Tengah"
+        },
+        {
+          "id": "CKT",
+          "name": "Kalimantan Timur"
+        },
+        {
+          "id": "CKU",
+          "name": "Kalimantan Utara"
+        },
+        {
+          "id": "CBL",
+          "name": "Bali"
+        },
+        {
+          "id": "CNB",
+          "name": "Nusa Tenggara Barat"
+        },
+        {
+          "id": "CNT",
+          "name": "Nusa Tenggara Timur"
+        }
+      ],
     }
   },
 }
